24efd9731314bdf04128eee6cae9a5b27ab0d9af
Fumitaka Tanaka <tanaka@ipride.co.jp>
2019-07-11 14:53:57 +0900

    [add] 永続化を実装した

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,50 @@ import "./App.css";
 import ToDoListItem from "./ToDoListItem";
 
 interface FormData {
-  todoList: { title: string; description: string }[]
+  todoList: { title: string ; description: string }[]
   title: string
   description: string
 }
 
 class App extends Component {
-  //ToDoListをstateに定義、初期値は[]
+  //ToDoListをstateに定義、初期値はlocalStrageから取得または []
   state: FormData = {
-    todoList: [], title: '', description: ''
+    todoList: JSON.parse(localStorage.getItem("todoList") as string) || [], title: '', description: ''
   };
+
+  //todoList itemの追加
+  addTodo = (item: {title: string, description: string}, callBack :() => void) => {
+    //todoList stateに追加
+    this.setState(
+      {
+        todoList: this.state.todoList.concat(item)
+      },
+      () => {
+        //localStorageにtodoList stateを保存
+        localStorage.setItem("todoList", JSON.stringify(this.state.todoList))
+        this.setState({
+          todoList: this.state.todoList
+        })
+        //callBack関数が引数に渡されていた場合に実行
+        callBack && callBack()
+      }
+    )
+  }
+
+  //todoListからitemを削除
+  removeTodo = (item: {title: string, description: string}/*, callBack*/) => {
+    this.setState(
+      {
+        todoList: this.state.todoList.filter(x => x !== item)
+      },
+      () => {
+        //localStorageにtodoList stateを保存
+        localStorage.setItem("todoList", JSON.stringify(this.state.todoList))
+        //callBack関数が引数に渡されていた場合に実行
+        //callBack && callBack()
+      }
+    )
+  }
 
   onChangeText(e: React.FormEvent<any>) {
     this.setState({...this.state, title: e.currentTarget.value})
@@ -30,7 +64,8 @@ class App extends Component {
           onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
             //formのでデフォルトのイベントをキャンセル
             e.preventDefault();
-
+            
+            /*
             //todoList stateに追加
             this.setState(
               (oldState: FormData) => {
@@ -42,8 +77,10 @@ class App extends Component {
                       title: oldState.title,
                       description: oldState.description
                     }
+                    
                   ]
                 };
+                
               },
               //stateの変更後に入力した値を空にする
               () => {
@@ -56,6 +93,27 @@ class App extends Component {
                 })
               }
             );
+            */
+            
+            //todoListに追加
+            this.addTodo(
+              //todoList stateに追加
+              {
+                title: this.state.title,
+                description: this.state.description
+              },
+              //stateの変更後に入力した値を空にする
+              () => {
+                this.setState((oldState: FormData) => {
+                  return {
+                    ...oldState,
+                    title: '',
+                    description: ''
+                  };
+                })
+              }
+            )
+
           }}
         >
           <div>
@@ -75,17 +133,9 @@ class App extends Component {
               title={todo.title}
               description={todo.description}
               //クリックされたItemをtodoList stateから削除
-              onClick={() => {
-                this.setState({
-                  todoList: this.state.todoList.filter(x => x !== todo)
-                })
-              }}
+              onClick={() => this.removeTodo(todo)}
             />
-            
-          ))
-          
-          }
-          
+          ))}
         </div>
       </div>
     );
